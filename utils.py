1a99eab56af82a596e48ae888cd786478fc36c21
hillscottc <scott289@gmail.com>
2014-07-24 12:53:22 -0700

    more

--- utils.py
+++ utils.py
@@ -1,5 +1,6 @@
 import requests
 from bs4 import BeautifulSoup
+import re
 
 URL_BASE = 'http://www.j-archive.com/showgame.php?game_id='
 
@@ -26,30 +27,36 @@ def parse_seasons(count=1):
     return game_ids
 
 
-def save_game(game_id):
-    url = URL_BASE + game_id
-    html = requests.get(url).text
-    outfile = open('samples/game_{}.html'.format(game_id), 'w')
-    outfile.write(html)
-    outfile.close()
+def save_games(*game_ids):
+    """Pull given games from remote source and save locally."""
+    for game_id in game_ids:
+        if isinstance(game_id, int):
+            game_id = str(game_id)
+        url = URL_BASE + game_id
+        html = requests.get(url).text
+        fname = 'samples/game_{}.html'.format(game_id)
+        with open(fname, "w") as outfile:
+            outfile.write(html)
+
+if __name__ == '__main__':
+    # save_games('xyz')
+    pass
 
 
-def save_games():
-    with open('samples/season_ids.txt', "r") as myfile:
-        for season_id in myfile:
-            save_game(season_id.replace('\n', ''))
-
-
-# if __name__ == '__main__':
-#     pass
-#     # save_games()
-
-
-
-# def get_page(game_id):
-#     url = URL_BASE + game_id
-#     return requests.get(url).text
-
+GAME_IDS = [4529, 368, 4249, 363, 362, 361, 360, 359, 356, 355, 354, 353, 352, 348, 347, 345,
+            344, 341, 340, 339, 338, 180, 179, 177, 174, 173, 3377, 4277, 1992, 1991, 4230,
+            4281, 4279, 2265, 2264, 2263, 2262, 2205, 4284, 4269, 116, 4271, 4260, 4258, 4254,
+            3952, 4267, 4273, 4256, 4261, 902, 900, 3069, 3067, 3065, 3062, 3061, 3060, 3053,
+            3051, 4251, 297, 294, 251, 295, 246, 4233, 3455, 3055, 4407, 4236, 4282, 4099, 4275,
+            1339, 1338, 1337, 1336, 1335, 3118, 3117, 4083, 4220, 4219, 4217, 4214, 4213, 4085,
+            1756, 3943, 4395, 3970, 4244, 4097, 4246, 4247, 4240, 3436, 3568, 4265, 4307, 4533,
+            4264, 629, 626, 2555, 3676, 3674, 3671, 3670, 3666, 2482, 3665, 4158, 4157, 3454,
+            3501, 3934, 3940, 2813, 4094, 4490, 4091, 2933, 4089, 4080, 4460, 3451, 3450, 4530,
+            4412, 3409, 3047, 4564, 4189, 4188, 4525, 4524, 4522, 4515, 4518, 4520, 4513, 4512,
+            2867, 1132, 1346, 1345, 1344, 1343, 1342, 4554, 2584, 2583, 4559, 115, 112, 105, 101,
+            93, 92, 84, 85, 83, 81, 4286, 4560, 4283, 1135, 4288, 1674, 1673, 1672, 1671, 1670,
+            4410, 4296, 4295, 4292, 4291, 4290, 4302, 3573, 4297, 4298, 3711, 182, 181, 178, 170,
+            171, 169, 167, 164, 160, 159, 3585, 4475, 4473, 4289, 4491]
 
 
 # outfile = open('season_ids.txt', 'w')
