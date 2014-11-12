92896d212c3e33b863e2eb4aa400a1647b830e59
Scott Hill <scott289@gmail.com>
2014-11-12 13:48:37 -0800

    heroku memcached

--- questproj/settings/shill.py
+++ questproj/settings/shill.py
@@ -12,13 +12,40 @@ NOSE_ARGS = [
     # '--with-fixture-bundling'
 ]
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'quest_db', 'USER': 'quest_acct', 'PASSWORD': '12345',
-        'HOST': 'localhost', 'PORT': '5432'
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': 'quest_db', 'USER': 'quest_acct', 'PASSWORD': '12345',
+#         'HOST': 'localhost', 'PORT': '5432'
+#     }
+# }
+
+import dj_database_url
+DATABASES = {'default': dj_database_url.config(default='postgres://localhost')}
+
+
+def get_cache():
+  import os
+  try:
+    os.environ['MEMCACHE_SERVERS'] = os.environ['MEMCACHIER_SERVERS'].replace(',', ';')
+    os.environ['MEMCACHE_USERNAME'] = os.environ['MEMCACHIER_USERNAME']
+    os.environ['MEMCACHE_PASSWORD'] = os.environ['MEMCACHIER_PASSWORD']
+    return {
+      'default': {
+        'BACKEND': 'django_pylibmc.memcached.PyLibMCCache',
+        'TIMEOUT': 500,
+        'BINARY': True,
+        'OPTIONS': { 'tcp_nodelay': True }
+      }
     }
-}
+  except:
+    return {
+      'default': {
+        'BACKEND': 'django.core.cache.backends.locmem.LocMemCache'
+      }
+    }
+
+CACHES = get_cache()
 
 
 
