8e5e3a26d77f72cb9a5f00f11073dc9c770ce05d
Scott Hill <scott289@gmail.com>
2014-11-15 06:42:12 -0800

    settings cleanup

--- questproj/settings/shill.py
+++ questproj/settings/shill.py
@@ -1,7 +1,6 @@
 from base import *
 import os
 
-import dj_database_url
 
 TEST_RUNNER = 'django_nose.NoseTestSuiteRunner'
 NOSE_ARGS = [
@@ -15,39 +14,28 @@ NOSE_ARGS = [
     # '--with-fixture-bundling'
 ]
 
-## local db
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-#         'NAME': 'quest_db', 'USER': 'quest_acct', 'PASSWORD': '12345',
-#         'HOST': 'localhost', 'PORT': '5432'
-#     }
-# }
+# define if on heroku environment
+ON_HEROKU = 'ON_HEROKU' in os.environ
 
-## Heroku db
-DATABASES = {'default': dj_database_url.config(default='postgres://localhost')}
+if ON_HEROKU:
+    import dj_database_url
+    DATABASES = {'default': dj_database_url.config(default='postgres://localhost')}
+
+    os.environ['MEMCACHE_SERVERS'] = os.environ['MEMCACHIER_SERVERS'].replace(',', ';')
+    os.environ['MEMCACHE_USERNAME'] = os.environ['MEMCACHIER_USERNAME']
+    os.environ['MEMCACHE_PASSWORD'] = os.environ['MEMCACHIER_PASSWORD']
+    CACHES = {'default': {'BACKEND': 'django_pylibmc.memcached.PyLibMCCache',
+                          'TIMEOUT': 500, 'BINARY': True,
+                          'OPTIONS': { 'tcp_nodelay': True }}}
+
+else:
+    DATABASES = {'default': {'ENGINE': 'django.db.backends.postgresql_psycopg2',
+                             'NAME': 'quest_db', 'USER': 'quest_acct', 'PASSWORD': '12345',
+                             'HOST': 'localhost', 'PORT': '5432'}}
+    CACHES = {'default': {'BACKEND': 'django.core.cache.backends.locmem.LocMemCache'}}
 
 
-def get_cache():
-    try:
-        os.environ['MEMCACHE_SERVERS'] = os.environ['MEMCACHIER_SERVERS'].replace(',', ';')
-        os.environ['MEMCACHE_USERNAME'] = os.environ['MEMCACHIER_USERNAME']
-        os.environ['MEMCACHE_PASSWORD'] = os.environ['MEMCACHIER_PASSWORD']
-        return {
-            'default': {
-                'BACKEND': 'django_pylibmc.memcached.PyLibMCCache',
-                'TIMEOUT': 500,
-                'BINARY': True,
-                'OPTIONS': { 'tcp_nodelay': True }
-            }
-        }
-    except:
-        return {
-            'default': {
-                'BACKEND': 'django.core.cache.backends.locmem.LocMemCache'
-            }
-        }
-CACHES = get_cache()
+
 
 
 
