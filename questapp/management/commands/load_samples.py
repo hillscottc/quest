fe7e4ba64cf82494726fbd6cb608e1bb9e23f351
Scott Hill <scott289@gmail.com>
2014-11-30 22:53:16 -0800

    manage load_samps with args

--- questapp/management/commands/load_samples.py
+++ questapp/management/commands/load_samples.py
@@ -1,8 +1,17 @@
-from django.core.management.base import NoArgsCommand
+from django.core.management.base import BaseCommand
 from questapp.utils import load_samples
 
-class Command(NoArgsCommand):
-    help = 'Parese and load all the html files.'
 
-    def handle_noargs(self, **options):
-        load_samples()
+class Command(BaseCommand):
+    args = '<num>'
+    help = 'Parse and load the html files. Defaults to all.'
+
+    def handle(self, *args, **options):
+        num = args[0] if args else None
+        load_samples(num)
+
+# class Command(NoArgsCommand):
+#     help = 'Parese and load all the html files.'
+#
+#     def handle_noargs(self, **options):
+#         load_samples()
