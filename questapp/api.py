dd16cad31f1592e93c3a52a30371176ddfe320ff
Scott Hill <scott289@gmail.com>
2014-12-16 13:30:37 -0800

    cleaner bootstrap list

--- questapp/api.py
+++ questapp/api.py
@@ -1,7 +1,8 @@
 from tastypie.resources import ModelResource
 from tastypie import fields
 from questapp.models import Clue, Category
-import questproj.cache_mgr as cache_mgr
+from tastypie.paginator import Paginator
+
 from questproj.utils import get_random_ids
 
 
@@ -27,3 +28,4 @@ class RandomCluesResource(ModelResource):
         rand_ids = get_random_ids(Clue, 1000)
         queryset = Clue.objects.filter(id__in=rand_ids)
         resource_name = 'random_clues'
+        paginator_class = Paginator
