2f368843428c6ab525068ba87322c714c485de4b
Scott Hill <scott289@gmail.com>
2014-12-18 11:45:34 -0800

    bb and django versions work :)

--- questproj/static/js/views/clue.js
+++ questproj/static/js/views/clue.js
@@ -7,49 +7,26 @@ app.ClueView = Backbone.View.extend({
     template: _.template( $('#clueTemplate').html() ),
 
     events: {
-        'click .clue': 'showAnswer'
+        'click .clue': 'clueClick'
     },
 
-    showAnswer: function(e) {
-        console.log("ok " + $(this).attr("className"));
-        console.log($(e.currentTarget).text())
+    clueClick: function(e) {
+        // Set target el class to active and write to answer el.
 
-        // This gets the ans el
-//        $(e.target.nextElementSibling)
+        var targ_el = $(e.currentTarget);
+        var ans_el = $(e.currentTarget).find('.answer');
 
-//        $(this).children('.XXanswer').toggle();
-//        $(this).children('.clue').children('.XXanswer').toggle();
-
-//        $(this).children('p').toggle(true);
-//        var ans_id = this.model.attributes['id'];
-//        console.log(ans_id)
-//        ans_el = $('#ans_'+ ans_id);
+        if (targ_el.hasClass("active")) {
+            // Remove active class, erase answer text
+            targ_el.removeClass("active");
+            ans_el.text('');
+        } else {
+            // Add active class, write answer text
+            targ_el.addClass("active");
+            ans_el.text(this.model.attributes['answer']);
+        }
         e.preventDefault();
     },
-
-//        $( '#addClue div' ).children( 'input' ).each( function( i, el ) {
-//            if( $( el ).val() != '' )
-//            {
-//                formData[ el.id ] = $( el ).val();
-//            }
-//            // Clear input field value
-//            $( el ).val('');
-//        });
-//
-//    <script type="text/javascript">
-//        $(document).ready(function() {
-//            $('.clue').click(function(e){
-//                $(this).children('.answer').toggle();
-//                if ($(this).hasClass("active")) {
-//                    $(this).removeClass("active");
-//                } else {
-//                    $(this).addClass("active");
-//                }
-//                e.preventDefault(); // dont want href action
-//            })
-//        });
-//    </script>
-
 
     render: function() {
         this.$el.html( this.template( this.model.attributes ) );
