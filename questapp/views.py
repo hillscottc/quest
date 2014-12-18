1a242c0c72bcfb75414d6300da7a31d7b942fdd5
Scott Hill <scott289@gmail.com>
2014-12-17 16:02:31 -0800

    minor changes

--- questapp/views.py
+++ questapp/views.py
@@ -51,6 +51,34 @@ class ClueRandomView(ListView):
         return Clue.objects.all().order_by('?')[:1000]
 
 
+class ClueSearchView(FormView):
+    context_object_name = 'clue_list'
+    template_name = "clue_search.html"
+    form_class = ClueSearchForm
+    paginate_by = 20
+
+    def form_valid(self, form):
+        cd = form.cleaned_data
+        context = self.get_context_data(form=form)
+
+        if not cd['search_txt']:
+            msg = "All recs."
+            qs = Clue.objects.all()
+        else:
+            msg = "Searching by: " + cd['search_txt']
+            qs = Clue.objects.filter(question__contains=cd['search_txt'])
+
+        max_recs = 50
+        clue_list = qs[:max_recs]
+
+        context.update({'test_msg': msg})
+        context.update({'list_type': 'clues-search'})
+        context.update({'clue_list': clue_list})
+        return self.render_to_response(context)
+
+    def get_success_url(self):
+        return reverse('clue-search')
+
 # class CluesByCatView(ListView):
 #     context_object_name = 'clue_list'
 #     template_name = 'clue_list.html'
@@ -82,28 +110,3 @@ class ClueRandomView(ListView):
 #         return queryset
 
 
-class ClueSearchView(FormView):
-    template_name = "clue_search.html"
-    form_class = ClueSearchForm
-
-    def form_valid(self, form):
-        cd = form.cleaned_data
-        context = self.get_context_data(form=form)
-
-        if not cd['search_txt']:
-            msg = "All recs."
-            qs = Clue.objects.all()
-        else:
-            msg = "Searching by: " + cd['search_txt']
-            qs = Clue.objects.filter(question__contains=cd['search_txt'])
-
-        max_recs = 50
-        clue_list = qs[:max_recs]
-
-        context.update({'test_msg': msg})
-        context.update({'list_type': 'clues-search'})
-        context.update({'clue_list': clue_list})
-        return self.render_to_response(context)
-
-    def get_success_url(self):
-        return reverse('clue-search')
