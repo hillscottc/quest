f0840b730b992603466a28b18d007e861bc07bf6
Scott Hill <scott289@gmail.com>
2014-12-18 13:25:00 -0800

    old code

--- questproj/static/js/app.js
+++ questproj/static/js/app.js
@@ -5,12 +5,3 @@ $(function() {
     new app.CluesView();
 });
 
-// $(function() {
-//     var books = [
-//         { title: 'JavaScript: The Good Parts', author: 'Douglas Crockford', releaseDate: '2008', keywords: 'JavaScript Programming' },
-//         { title: 'American Psycho', author: 'Bret Easton Ellis', releaseDate: '1991', keywords: 'Novel Splatter' },
-//         { title: 'Eloquent JavaScript', author: 'Marijn Haverbeke', releaseDate: '2011', keywords: 'JavaScript Programming' }
-//     ];
-
-//     new app.LibraryView( books );
-// });
