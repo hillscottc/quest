b716766b98661a7eb9f4819cfbf5e87980c0532b
Scott Hill <scott289@gmail.com>
2014-12-19 00:06:38 -0800

    use cor to expose api

--- questapp/api.py
+++ questapp/api.py
@@ -2,18 +2,19 @@ from tastypie.resources import ModelResource
 from tastypie import fields
 from questapp.models import Clue
 from tastypie.paginator import Paginator
+from questapp.cors_base import CORSModelResource
 
 from questproj.utils import get_random_ids
 
 
-class ClueResource(ModelResource):
+class ClueResource(CORSModelResource):
     class Meta:
         queryset = Clue.objects.all()
         resource_name = 'clue'
         paginator_class = Paginator
 
 
-class RandomCluesResource(ModelResource):
+class RandomCluesResource(CORSModelResource):
 
     class Meta:
         queryset = Clue.objects.all().order_by('?')[:100]
