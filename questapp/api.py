599a08f16eea85fb080271861d2c65ce39c9d070
Scott Hill <scott289@gmail.com>
2014-12-29 15:36:53 -0800

    comments

--- questapp/api.py
+++ questapp/api.py
@@ -1,6 +1,8 @@
 from questapp.models import Clue
 from questapp.cors_base import CORSModelResource
 
+# Note: API_LIMIT_PER_PAGE is set in settings/base.py
+
 
 class ClueResource(CORSModelResource):
     class Meta:
