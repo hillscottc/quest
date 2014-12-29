ba60817eb1cec3681400ba0bd58fdee06c495e0f
Scott Hill <scott289@gmail.com>
2014-12-29 13:24:24 -0800

    dont need paging here

--- questapp/api.py
+++ questapp/api.py
@@ -1,17 +1,11 @@
-from tastypie.resources import ModelResource
-from tastypie import fields
 from questapp.models import Clue
-from tastypie.paginator import Paginator
 from questapp.cors_base import CORSModelResource
-
-from questproj.utils import get_random_ids
 
 
 class ClueResource(CORSModelResource):
     class Meta:
         queryset = Clue.objects.all()
         resource_name = 'clue'
-        paginator_class = Paginator
 
 
 class RandomCluesResource(CORSModelResource):
