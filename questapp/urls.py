79830f429dc0a704cadc599bd826171046012e8d
Scott Hill <scott289@gmail.com>
2015-01-10 22:14:52 -0800

    added user and userprof to api

--- questapp/urls.py
+++ questapp/urls.py
@@ -2,26 +2,21 @@ from django.conf.urls import patterns, url, include
 from django.contrib import admin
 from questapp import views
 from tastypie.api import Api
-from questapp.api import ClueResource, RandomCluesResource
+from questapp.api import (ClueResource, RandomCluesResource,
+                          UserResource, UserProfileResource)
 
 admin.autodiscover()
 
 v1_api = Api(api_name='v1')
 v1_api.register(ClueResource())
 v1_api.register(RandomCluesResource())
+v1_api.register(UserResource())
+v1_api.register(UserProfileResource())
 
 
 urlpatterns = patterns(
     '',
-
     url(r'^clues/search/$', views.ClueSearchView.as_view(), name='clue-search'),
-
     url(r'^api/', include(v1_api.urls)),
-
 )
 
-# Sample API Urls:
-# /api/v1/?format=json
-# /api/v1/clue?format=json
-# /api/v1/clue/1/?format=json
-# /api/v1/random_clues?format=json
