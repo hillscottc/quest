2de67ed21a50ec7bd835335182f1bbc769722248
Scott Hill <scott289@gmail.com>
2015-01-10 22:14:07 -0800

    added user and userprof to api

--- questapp/api.py
+++ questapp/api.py
@@ -1,10 +1,14 @@
-from questapp.models import Clue
+from django.contrib.auth.models import User
+from questapp.models import Clue, UserProfile
 from questapp.cors_base import CORSModelResource
-
+from tastypie import fields
+from tastypie.resources import ALL, ALL_WITH_RELATIONS
+from tastypie.authorization import Authorization
 # Note: API_LIMIT_PER_PAGE is set in settings/base.py
 
 
 class ClueResource(CORSModelResource):
+
     class Meta:
         queryset = Clue.objects.all()
         resource_name = 'clue'
@@ -15,3 +19,27 @@ class RandomCluesResource(CORSModelResource):
     class Meta:
         queryset = Clue.objects.all().order_by('?')[:100]
         resource_name = 'random_clues'
+
+
+class UserResource(CORSModelResource):
+
+    class Meta:
+        queryset = User.objects.all()
+        resource_name = 'user'
+        excludes = ['password', 'is_active', 'is_staff', 'is_superuser']
+        filtering = {
+            'username': ALL,
+        }
+
+
+class UserProfileResource(CORSModelResource):
+
+    user = fields.ForeignKey(UserResource, 'user', full=True)
+
+    class Meta:
+        queryset = UserProfile.objects.all()
+        resource_name = 'user_prof'
+        authorization = Authorization()
+        filtering = {
+            'user': ALL_WITH_RELATIONS,
+        }
