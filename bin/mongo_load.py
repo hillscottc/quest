6de51213fafcd2b99b1a483ed4829117318d3729
Scott Hill <scott289@gmail.com>
2015-01-31 13:43:08 -0800

    load random clues to mongo

--- bin/mongo_load.py
+++ bin/mongo_load.py
@@ -2,17 +2,17 @@ from pymongo import MongoClient
 from questapp.models import Clue
 
 
-def load_mongo():
+def load_mongo(num=1000):
+    """ Inits the mongo questdb with random clues from the default db.
+    """
     client = MongoClient()
-    db = client.quest_db
+    mongo_db = client.quest_db
+    mongo_db.clues.drop()
 
-    db.clues.drop()
+    for clue in Clue.objects.all().order_by('?')[:num]:
+        mongo_db.clues.insert({'question': clue.question,
+                               'answer': clue.answer,
+                               'category': clue.category})
 
-    for pg_clue in Clue.objects.all()[:1000]:
-        db.clues.insert({'question': pg_clue.question,
-                         'answer': pg_clue.answer,
-                         'category': pg_clue.category})
-
-    print [clue for clue in db.clues.find()]
-
+    print "Mongo records:", mongo_db.clues.count()
 
