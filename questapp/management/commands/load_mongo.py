f849cdfbd2d3d777ddf1bccfa4a7109ec3f6ea4f
Scott Hill <scott289@gmail.com>
2015-02-02 09:23:14 -0800

    using a mgmt cmd for load_monbgo

---
+++ questapp/management/commands/load_mongo.py
@@ -0,0 +1,28 @@
+from django.core.management.base import BaseCommand
+from pymongo import MongoClient
+from questapp.models import Clue
+
+
+class Command(BaseCommand):
+    """ Inits the mongo questdb with random clues from the default db.
+    """
+    args = '<num>'
+    help = 'Inits the mongo questdb with <num> random clues from the default db'
+    default_num = 1000
+
+    def handle(self, *args, **options):
+        num = args[0] if args else self.default_num
+
+        client = MongoClient()
+        mongo_db = client.quest_db
+
+        mongo_db.clues.drop()
+
+        for clue in Clue.objects.all().order_by('?')[:num]:
+            mongo_db.clues.insert({'question': clue.question,
+                                   'answer': clue.answer,
+                                   'category': clue.category})
+
+        print "Mongo records:", mongo_db.clues.count()
+
+
