2245d688296c24ccc4af815d21de8f0706a2f796
Scott Hill <scott289@gmail.com>
2015-04-11 19:51:59 -0700

    practice mode

--- questproj/static/js/clues.js
+++ questproj/static/js/clues.js
@@ -55,8 +55,49 @@ $(".tellme-btn").click(function(e) {
     e.preventDefault();
 });
 
+// Check guess click if not in practice mode.
+$(".guess-btn").click(function(e) {
+    var answer = $(this).siblings('.hidden-answer').val();
+    var questionid = $(this).siblings('.hidden-questionid').val();
+    var userid = $('#hidden-userid').val();
+    var results_el = $(this).siblings('.results');
+    var guess_el = $(this).siblings('.guess-text');
 
+    if (fuzzyMatch(guess_el.val(), answer)) {
+        results_el.text("Right!");
+        guess_el.val(answer);
+
+        // Post to the log
+        var data = {'userid': userid, 'questionid': questionid};
+        ajaxPost('/userlog/post', data);
+
+        // Update user today display
+        var count_el = $('#count-user-today');
+        var count = count_el.text();
+        count_el.text(++count);
+
+        // Update user all display
+        count_el = $('#count-user-all');
+        count = count_el.text();
+        count_el.text(++count);
+
+        // Disable further edit
+        guess_el.prop("readonly", true);
+
+    } else {
+        results_el.text("Sorry, no.");
+    }
+
+    e.preventDefault();
+});
+
+// Live checking if in practice mode.
 $(".guess-text").on('input', function() {
+
+    if($('#practice-mode').is(':checked') == false) {
+        return;
+    }
+
     var answer = $(this).siblings('.hidden-answer').val();
     var questionid = $(this).siblings('.hidden-questionid').val();
     var userid = $('#hidden-userid').val();
@@ -89,3 +130,10 @@ $(".guess-text").on('input', function() {
 });
 
 
+$("#practice-mode").change(function() {
+    if(this.checked) {
+        $('.guess-btn').hide();
+    } else {
+        $('.guess-btn').show();
+    }
+});
