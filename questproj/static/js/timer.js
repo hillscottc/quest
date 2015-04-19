7c663d5ec6c5c106be898daaac25003e10174b1e
Scott Hill <scott289@gmail.com>
2015-04-18 18:43:04 -0700

    timer pause

--- questproj/static/js/timer.js
+++ questproj/static/js/timer.js
@@ -1,20 +1,38 @@
 
-// Init the timer
 $(function () {
+
+    // Init the timer
     $('#timer').countdown({format: 'mS',
         // expiryText: '<div class="timesup">Time is up!</div>',
         onExpiry: liftOff
     });
+
+    // Disable the pause
+    $('#timerPause').prop("disabled", true);
+
 });
 
 $('#timerGo').click(function() {
+
+    // Enable the pause
+    $('#timerPause').prop("disabled", false);
+
     var startTime = new Date();
     var secs = parseInt($('#timerChoice').val());
     startTime.setSeconds(startTime.getSeconds() + secs);
     $('#timer').countdown('option', {until: startTime});
 });
 
+$('#timerPause').click(function() {
+
+    var pause = $(this).text() === 'pause';
+    $(this).text(pause ? 'resume' : 'pause');
+    $('#timer').countdown(pause ? 'pause' : 'resume');
+
+});
+
 function liftOff() {
-    console.log('Do something!!!');
+    console.log('The time is up!');
+    $('#timerPause').prop("disabled", true);
 }
 
