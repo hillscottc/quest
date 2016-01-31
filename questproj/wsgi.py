57dcc2a03f2252fb5357e15b6c7f5579b234bf1c
Scott Hill <scott289@gmail.com>
2016-01-30 21:47:54 -0800

    whitenoise?

--- questproj/wsgi.py
+++ questproj/wsgi.py
@@ -9,11 +9,12 @@ https://docs.djangoproject.com/en/1.6/howto/deployment/wsgi/
 
 import os
 from django.core.wsgi import get_wsgi_application
-from dj_static import Cling
+# from dj_static import Cling
+from whitenoise.django import DjangoWhiteNoise
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "questproj.settings")
 
-from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
 
-application = Cling(get_wsgi_application())
+# application = Cling(get_wsgi_application())
+application = DjangoWhiteNoise(application)
